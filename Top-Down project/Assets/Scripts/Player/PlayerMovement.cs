0f53e15c141b035048be1a8946072502b81dd4a6
Jason Sivak <20.jason.sivak@ksd.org>
2018-10-02 14:00:43 -0700

    Working rotation

---
+++ Top-Down project/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    //allows the user to deside individual speed values for horizontal and vertical movements
+    public float HSpeed;
+    public float VSpeed;
+
+    
+
+    float x;
+    float y;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        x = Input.GetAxisRaw("Horizontal");
+        y = Input.GetAxisRaw("Vertical");
+
+        if (x >= 0.5)
+        {
+           
+            transform.Rotate(0f, 0f, 5f);
+        }else if (x <= -0.5)
+        {
+
+            transform.Rotate(0f, 0f, -5f);
+        }
+
+        if (y == 1)
+        {
+            // GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 5f);
+            add force;
+        }
+    }
+
+}
