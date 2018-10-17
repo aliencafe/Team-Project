6c6fce4cd74db937bbfae003f58a17b3ea0cc4b9
Gwendolyn Farrar <19.gwendolyn.farrar@ksd.org>
2018-10-17 13:51:42 -0700

    ground control to major tom

---
+++ Top-Down project/Assets/Scripts/LoadWin.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadWin : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            //assuming we're attaching this script to the object
+            //at the end of the level
+            SceneManager.LoadScene("Win");
+        }
+	}
+}
---
+++ Top-Down project/Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour {
+
+	public void NewGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
