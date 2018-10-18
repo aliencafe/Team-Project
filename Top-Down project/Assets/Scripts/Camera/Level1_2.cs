ce9260565031e15d3e462649bcc74304fcf476bc
Jason Sivak <20.jason.sivak@ksd.org>
2018-10-18 12:58:35 -0700

    King candy loves candy

---
+++ Top-Down project/Assets/Scripts/Camera/Level1_2.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level1_2 : MonoBehaviour {
+
+    public GameObject Player;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == Player)
+
+            SceneManager.LoadScene("Level2");
+
+    }
+
+}
+
---
+++ Top-Down project/Assets/Scripts/Enemy/Death2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death2 : MonoBehaviour {
+
+    public int Health = 5;
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+
+        {
+
+            Health = Health - 1;
+            Destroy(collision.gameObject);
+            Debug.Log("I'm at " + Health);
+            if (Health <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+    }
+}
